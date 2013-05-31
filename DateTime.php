244001cc2d82be0bad40f75fd19d3dc8c7ff3148
fibbarth <fibbarth@gmail.com>
2013-05-31 16:59:52 -0300

    Atualizado arquivo

--- DateTime.php
+++ DateTime.php
@@ -3,23 +3,23 @@
  * @author Felipe Barth http://felipebarth.com.br/blog
  * @date 2013-05-31
  * 
- * Aqui vamos demostrar como realizar operações de adição 
- * subtração e diferenças de datas usando a Classe nativa do PHP
+ * Exemplo de como realizar operações de adição 
+ * subtração e diferenças de datas usando a Classe nativa do PHP DateTime
  * e a classe DateInterval.
  * 
  * Referências
  * http://www.php.net/manual/en/class.datetime.php
  * http://www.php.net/manual/en/class.dateinterval.php
  */
-
+	
 	//Primeiro criamos um objeto DateTime com a data de hoje ( 31/05/2013 )
-	$hoje = new DateTime('now');
+	$hoje = new DateTime( 'now', new DateTimeZone( 'America/Sao_Paulo') );
 
 	/**
 	 * Usamos o método format apenas para confirmar que está com a data de hoje
 	 * http://php.net/manual/en/datetime.format.php
 	 */  
-	echo $hoje->format('d/m/Y H:i:s');
+	echo $hoje->format('d/m/Y');
 	
 	// Criado intervalo de dois dias http://www.php.net/manual/en/dateinterval.createfromdatestring.php
 	// Pode ser criado desta forma
@@ -30,6 +30,12 @@
 	
 	// Para confirmar que ambos são iguais
 	var_dump('<pre>',$intervalo, $itervalo2,'</pre>');
+	
+	//Substraindo dois dias do objeto hoje
+	$hoje->sub($intervalo);
+	echo $hoje->format('d/m/Y');
+	
+	
 	
 	
 ?>
