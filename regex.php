0ff295a45503f8f3352d3793de11f53cc16d9a12
Felipe Ismael Barth <fibbarth@gmail.com>
2015-02-25 14:25:00 -0300

    Update regex.php
    fix white spaces

--- regex.php
+++ regex.php
@@ -4,14 +4,14 @@
 	 * Exemplo de uso da função preg_replace.
 	 */
 	$nomes = array(	'Fulano da Silva Inscrito',
-					'Fulano da Silva *** inscrito***',
-					'Fulano da Silva ** inscrito',
-					'Fulano da Silva (INSCRITO)',
-					'Fulano da Silva *(inscrito)**',
-					'Fulano da Silva * inscrito*',	
-					'Fulano da Silva * inscritos *****',
-					'Fulano da Silva ( inscritos *****',
-				   );
+			'Fulano da Silva *** inscrito***',
+			'Fulano da Silva ** inscrito',
+			'Fulano da Silva (INSCRITO)',
+			'Fulano da Silva *(inscrito)**',
+			'Fulano da Silva * inscrito*',	
+			'Fulano da Silva * inscritos *****',
+			'Fulano da Silva ( inscritos *****',
+		      );
 	
 	foreach( $nomes as $nome ){
 		$nArrumado[] = preg_replace('/[*([:blank:]]*(inscrito)s?[[:blank:]*)]*/i','',$nome);
