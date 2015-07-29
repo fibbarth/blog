ce156e97aeae48b6bebba6e8477d722676084a6a
Felipe Ismael Barth <fibbarth@gmail.com>
2015-07-29 14:55:06 -0300

    Create add_text_copy.js

---
+++ add_text_copy.js
@@ -0,0 +1,22 @@
+function addLink() {
+    //Get the selected text and append the extra info
+    var selection = window.getSelection(),
+        pagelink = '<br /><br /> Redad more at: ' + document.location.href,
+        copytext = selection + pagelink,
+        newdiv = document.createElement('div');
+
+    //hide the newly created container
+    newdiv.style.position = 'absolute';
+    newdiv.style.left = '-99999px';
+
+    //insert the container, fill it with the extended text, and define the new selection
+    document.body.appendChild(newdiv);
+    newdiv.innerHTML = copytext;
+    selection.selectAllChildren(newdiv);
+
+    window.setTimeout(function () {
+        document.body.removeChild(newdiv);
+    }, 100);
+}
+
+document.addEventListener('copy', addLink);
