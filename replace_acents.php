e40633493c99993efdb2033b5bd783030c1a606b
Felipe Ismael Barth <fibbarth@gmail.com>
2015-10-22 14:15:21 -0200

    Update replace_acents.php

--- replace_acents.php
+++ replace_acents.php
@@ -2,7 +2,7 @@
  
 function replace_accents($str) {
 $str = htmlentities($str, ENT_COMPAT, 'UTF-8');
-$str = preg_replace('/&([a-zA-Z])(uml|acute|grave|circ|tilde);/', '$1', $str);
+$str = preg_replace('/&([a-zA-Z])(uml|acute|grave|circ|tilde|cedil);/', '$1', $str);
  
 return html_entity_decode($str);
 }
